d28cac53671b2b15db0de64fc6bba82226a18ae3
SashaBusinaro <businaro.sasha@gmail.com>
2023-11-26 22:57:00 +0100

    react basics

---
+++ react/app/src/Component.js
@@ -0,0 +1,11 @@
+function Component(props) {
+  return (
+    <p>
+      {" "}
+      This is a component with color {props.color} and must be written with the
+      first letter capitalized
+    </p>
+  );
+}
+
+export default Component;
--- react/app/src/index.js
+++ react/app/src/index.js
@@ -1,7 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Component from "./Component";
 
-const myFirstElement = <h1>Hello React!</h1>
+const myFirstElement = <h1>Hello React!</h1>;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+let name = "there";
+const mySecondElement = <h2> Hi {name} ! </h2>;
+
+<> This is a fragment </>;
+
+let color = "blue";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    {myFirstElement}
+    {mySecondElement}
+    <Component color={color} />
+  </>
+);

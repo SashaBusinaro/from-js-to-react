b727b6e45130284cab807fa53ee2af6d95f4f8a2
SashaBusinaro <businaro.sasha@gmail.com>
2023-11-26 22:14:29 +0100

    added var_let_const comparison

---
+++ es6/var_let_const.js
@@ -0,0 +1,9 @@
+var x = 817
+// var has a function scope, not a block scope.
+
+let y = 267
+// let has a block scope.
+
+const z = 198
+// const has a block scope.
+// It does not define a constant value. It defines a constant reference to a value.
